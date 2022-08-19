1b8d189900157b238e4a02d833692115c3d13671
Danila Moriakov <d.moriakov@mail.ru>
2022-08-19 13:06:07 +0300

    + migrations

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 from auth.router import router as auth_router
 from ads.router import router as ads_router
-from common.database import engine, Base, create_database_if_not_exists
-
-create_database_if_not_exists(engine)
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
